2695410082baa5ed88b5f470886b70a6cb03bc96
mbranting <39920036+mbranting@users.noreply.github.com>
2020-01-09 09:56:03 -0700

    Add files via upload

---
+++ lbs_to_kilo.cpp
@@ -0,0 +1,13 @@
+//McKenna Branting
+//This is my own work 
+#include <iostream>
+using namespace std;
+int main() {
+	//user enters number in pounds
+	cout << "What is the number in pounds?" << endl;
+	double pounds;
+	cin >> pounds;
+	cout << "The number in kilograms is:" << endl;
+	cout << pounds * 0.454 << endl;
+	return 0;
+}
